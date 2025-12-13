75f4c70964a1f0e8557d360ab4f72f6440e0502d
cupofwonder <cupofwonder@yandex.ru>
2025-12-13 17:29:26 +0200

    Returned the classical problem statement

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -4,14 +4,11 @@ from item import Item
 from gilded_rose import GildedRose
 
 
-@pytest.fixture
-def gilded_rose():
-    return GildedRose()
 
-
-def test_regular_items_decrease_by_one(gilded_rose):
+def test_regular_items_decrease_by_one():
     items = [Item("+5 Dexterity Vest", 10, 20)]
-    gilded_rose.update_quality(items)
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
 
     item = items[0]
     assert item.sell_in == 9
@@ -26,10 +23,11 @@ def test_regular_items_decrease_by_one(gilded_rose):
     ],
 )
 def test_quality_goes_up_for_improving_products(
-    gilded_rose, name, sell_in, quality, expected_sell_in, expected_quality
+    name, sell_in, quality, expected_sell_in, expected_quality
 ):
     items = [Item(name, sell_in, quality)]
-    gilded_rose.update_quality(items)
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
 
     item = items[0]
     assert item.sell_in == expected_sell_in
@@ -39,15 +37,15 @@ def test_quality_goes_up_for_improving_products(
 @pytest.mark.parametrize(
     "name,sell_in,quality,expected_sell_in,expected_quality",
     [
-        ("Aged Brie", 10, 34, 9, 36),
         ("Backstage passes to a TAFKAL80ETC concert", 8, 30, 7, 32),
     ],
 )
-def test_quality_goes_up_by_two_for_improving_products_with_10_days_or_less_left(
-    gilded_rose, name, sell_in, quality, expected_sell_in, expected_quality
+def test_quality_goes_up_by_two_for_backstage_products_with_10_days_or_less_left(
+    name, sell_in, quality, expected_sell_in, expected_quality
 ):
     items = [Item(name, sell_in, quality)]
-    gilded_rose.update_quality(items)
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
 
     item = items[0]
     assert item.sell_in == expected_sell_in
@@ -57,15 +55,15 @@ def test_quality_goes_up_by_two_for_improving_products_with_10_days_or_less_left
 @pytest.mark.parametrize(
     "name,sell_in,quality,expected_sell_in,expected_quality",
     [
-        ("Aged Brie", 4, 11, 3, 14),
         ("Backstage passes to a TAFKAL80ETC concert", 5, 15, 4, 18),
     ],
 )
-def test_quality_goes_up_by_three_for_improving_products_with_5_days_or_less_left(
-    gilded_rose, name, sell_in, quality, expected_sell_in, expected_quality
+def test_quality_goes_up_by_three_for_backstage_products_with_5_days_or_less_left(
+    name, sell_in, quality, expected_sell_in, expected_quality
 ):
     items = [Item(name, sell_in, quality)]
-    gilded_rose.update_quality(items)
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
 
     item = items[0]
     assert item.sell_in == expected_sell_in
@@ -80,10 +78,11 @@ def test_quality_goes_up_by_three_for_improving_products_with_5_days_or_less_lef
     ],
 )
 def test_quality_and_sellin_decrease_twice_as_fast_after_sell_by(
-    gilded_rose, name, sell_in, quality, expected_sell_in, expected_quality
+    name, sell_in, quality, expected_sell_in, expected_quality
 ):
     items = [Item(name, sell_in, quality)]
-    gilded_rose.update_quality(items)
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
 
     item = items[0]
     assert item.sell_in == expected_sell_in
@@ -93,33 +92,35 @@ def test_quality_and_sellin_decrease_twice_as_fast_after_sell_by(
 @pytest.mark.parametrize(
     "name,sell_in,quality,expected_sell_in,expected_quality",
     [
-        ("Aged Brie", 0, 20, -1, 0),
         ("Backstage passes to a TAFKAL80ETC concert", 0, 20, -1, 0),
     ],
 )
-def test_backstage_passes_and_brie_go_to_quality_zero_after_sell_by(
-    gilded_rose, name, sell_in, quality, expected_sell_in, expected_quality
+def test_backstage_passes_go_to_quality_zero_after_sell_by(
+    name, sell_in, quality, expected_sell_in, expected_quality
 ):
     items = [Item(name, sell_in, quality)]
-    gilded_rose.update_quality(items)
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
 
     item = items[0]
     assert item.sell_in == expected_sell_in
     assert item.quality == expected_quality
 
 
-def test_sulfuras_the_immutable(gilded_rose):
+def test_sulfuras_the_immutable():
     items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
-    gilded_rose.update_quality(items)
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
 
     item = items[0]
     assert item.sell_in == 0
     assert item.quality == 80
 
 
-def test_quality_does_not_increase_past_50(gilded_rose):
-    items = [Item("Aged Brie", 4, 49)]
-    gilded_rose.update_quality(items)
+def test_quality_does_not_increase_past_50():
+    items = [Item("Aged Brie", 4, 50)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
 
     item = items[0]
     assert item.sell_in == 3
@@ -127,9 +128,10 @@ def test_quality_does_not_increase_past_50(gilded_rose):
 
 
 @pytest.mark.skip(reason="TODO: implement conjured rule")
-def test_conjured_items_decrease_in_quality_twice_as_fast(gilded_rose):
+def test_conjured_items_decrease_in_quality_twice_as_fast():
     items = [Item("Conjured Mana Cake", 3, 6)]
-    gilded_rose.update_quality(items)
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
 
     item = items[0]
     assert item.sell_in == 2
